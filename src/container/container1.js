1bf97dd80dbc351b385087c7b6c6bd62bfb35269
hridoymahmud71 <hridoymahmud71@gmail.com>
2019-11-24 18:51:41 +0600

    Sun, Nov 24, 2019  6:51:41 PM

--- src/container/container1.js
+++ src/container/container1.js
@@ -1,10 +1,54 @@
 import React, { Component } from 'react'
 
 class Container1 extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            level: "init",
+            power:1
+        }
+    }
+
+    changeLevelState = () => {
+        this.setState(
+            () =>
+                (
+                    {
+                        level: this.state.level + " "+ "medium"
+                    }
+                )
+        )
+    }
+    changePowerState = () => {
+        this.setState(
+            () =>
+                (
+                    {
+                        power: this.state.power + 1
+                    }
+                )
+        )
+    }
+    bothState = () => {
+        this.setState(
+            () =>
+                (
+                    {
+                        level: this.state.level + " "+ "medium",
+                        power: this.state.power + 1
+                    }
+                )
+        )
+    }
+
     render() {
         return (
             <div>
-                max is  {this.props.max} and type is {this.props.nick}
+                <button onClick={() => this.changeLevelState()}>Change Level</button>
+                <button onClick={() => this.changePowerState()}>Change Power</button>
+                <button onClick={() => this.bothState()}>Change Both</button>
+                level  is {this.state.level} and power is {this.state.power}
             </div>
         );
     }
