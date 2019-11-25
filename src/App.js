78505b2e5e94545349ac3ce882ca1c563efcb6c1
hridoymahmud71 <hridoymahmud71@gmail.com>
2019-11-25 14:28:29 +0600

    Mon, Nov 25, 2019  2:28:29 PM

--- src/App.js
+++ src/App.js
@@ -1,16 +1,42 @@
 import React, { Component } from 'react';
-import  Component1 from './functional/component1'
-import  Container1 from './container/container1'
+// import Component1 from './functional/component1'
+//  import Container1 from './container/container1'
 
 
 
 class App extends Component {
-  
-  render() {
+  // cities = [
+  //   { id: 1, post: "jakarta" },
+  //   { id: 2, post: "bali" },
+  //   { id: 3, post: "rangoon" },
+  //   { id: 4, post: "pataya" },
+  //   { id: 5, post: "hanoi" },
+  //   { id: 6, post: "chiang mai" },
+  //   { id: 7, post: "kualalampur" },
+  // ]
+
+  // renderCities = (props) => (
+  //   <div>
+  //     <p>
+  //       {props.city.post}
+  //     </p>
+  //     <p>
+  //       {props.city.id}
+  //     </p>
     
+  //   </div>
+  // )
+    
+
+
+  render() {
+
     return (
       <div className="App">
-       <Container1 nick="fury" max="mad" />  
+        {/* {
+        this.cities.map((city,idx) => (<this.renderCities city={city} key={idx}/>)) 
+        }
+        <Container1 nick="fury" max="mad" />   */}
       </div>
     );
   }
